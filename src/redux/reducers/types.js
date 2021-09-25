9f702150d6ae71339987a56884bb2e00115c5965
Melih00 <melihagilli0@gmail.com>
2021-09-25 23:37:09 +0300

    all clear

--- src/redux/reducers/types.js
+++
@@ -1,3 +0,0 @@
-export const USER_LOGIN = 'USER_LOGIN'
-export const POSTS = 'POSTS'
-export const SHOW_WELCOME = 'SHOW_WELCOME'
--- src/styling/styling.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const ModalImg = styled.img`
-
-width: 100%;
-height: auto;
-border-radius: 3%;
-`;
