343d0e875179fad3eaa98026b4906755feb5e95e
Melih00 <melihagilli0@gmail.com>
2021-09-25 17:25:09 +0300

    .env updated

--- melihmedia/src/firebase/firebase.js
+++ melihmedia/src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
-  databaseURL:process.env.REACT_APP_databaseURL,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL:process.env.REACT_APP_DATA_BASE_URL,
 }
 const app = firebase.initializeApp(firebaseConfig);
 const auth = app.auth();
